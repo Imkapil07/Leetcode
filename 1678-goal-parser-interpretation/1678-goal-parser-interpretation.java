84d124794823db445426263f5e54ef59f77fb243
Kapil Rai <116628083+Imkapil07@users.noreply.github.com>
2024-02-21 00:33:06 +0530

    Time: 1 ms (74.34%), Space: 41.5 MB (66.04%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,7 +1,7 @@
 class Solution {
     public String interpret(String command) {
-        String s = command.replace("()","o");
-        String s1 = s.replace("(al)","al");
-        return s1;
+        String s = command.replace("()","o").replace("(al)","al");
+        // String s1 = s.replace("(al)","al");
+        return s;    
     }
 }
